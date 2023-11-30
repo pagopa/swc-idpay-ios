704f50bb1bcb0d955399cc414156e2495eb344d3
stefaniacastiglioni <stefania.castiglioni@mobilesoft.it>
2023-11-30 16:27:19 +0100

    Change swift-tools-version and add platform to Package.swift

--- PagoPAUIKit/Package.swift
+++ PagoPAUIKit/Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "PagoPAUIKit",
+    platforms: [.iOS(.v16)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
