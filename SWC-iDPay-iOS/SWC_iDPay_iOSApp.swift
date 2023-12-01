3f8416ae9e3910135d4758f89e4a25e93c42d8c5
stefaniacastiglioni <stefania.castiglioni@mobilesoft.it>
2023-12-01 18:22:48 +0100

    Load Components demo as main app view

--- SWC-iDPay-iOS/SWC_iDPay_iOSApp.swift
+++ SWC-iDPay-iOS/SWC_iDPay_iOSApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SWC_iDPay_iOSApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ComponentsDemoListView()
         }
     }
 }
