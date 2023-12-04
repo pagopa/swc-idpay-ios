239b16e33ee78519b081f8282dc87e314172e98a
stefaniacastiglioni <stefania.castiglioni@mobilesoft.it>
2023-12-04 11:06:38 +0100

    Add Divider component

---
+++ PagoPAUIKit/Sources/PagoPAUIKit/Divider/Divider.swift
@@ -0,0 +1,58 @@
+//
+//  Divider.swift
+//  
+//
+//  Created by Stefania Castiglioni on 29/11/23.
+//
+
+import SwiftUI
+
+struct Divider: View {
+    
+    var orientation: Orientation = .horizontal
+    var color: Color = .grey50
+    
+    enum Orientation {
+        case horizontal, vertical
+    }
+    
+    var body: some View {
+        Rectangle()
+            .frame(maxWidth: orientation == .horizontal ? .infinity : 1 , maxHeight: orientation == .horizontal ? 1 : .infinity)
+        .background(color)
+        .foregroundColor(.clear)
+    }
+}
+
+struct Divider_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        ScrollView {
+            VStack {
+                Text("Prova")
+                
+                Divider(orientation: .horizontal)
+                    .padding(Spacings.medium.rawValue)
+                
+                Text("Horizontal divider")
+            }
+            .frame(maxWidth: .infinity)
+            .padding(40)
+            .background(Color.successLight)
+            
+            HStack(alignment: .center) {
+                Text("Prova")
+                    .frame(maxWidth: .infinity)
+
+                
+                Divider(orientation: .vertical, color: .infoGraphic)
+                    .padding(Spacings.medium.rawValue)
+                
+                Text("Vertical divider")
+                    .frame(maxWidth: .infinity)
+            }
+            .frame(height: 120)
+            .background(Color.infoLight)
+        }
+    }
+}
