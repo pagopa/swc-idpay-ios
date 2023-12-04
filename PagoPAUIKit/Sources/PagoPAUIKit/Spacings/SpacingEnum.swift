cc3ef6707332705647c54a7bd0b878e600193527
stefaniacastiglioni <stefania.castiglioni@mobilesoft.it>
2023-12-04 10:59:56 +0100

    Add spacings enum

---
+++ PagoPAUIKit/Sources/PagoPAUIKit/Spacings/SpacingEnum.swift
@@ -0,0 +1,17 @@
+//
+//  SpacingEnum.swift
+//  
+//
+//  Created by Stefania Castiglioni on 29/11/23.
+//
+
+import Foundation
+
+public enum Spacings: CGFloat {
+    case xsmall     = 8.0
+    case small      = 16.0
+    case medium     = 24.0
+    case large      = 32.0
+    case xlarge     = 40.0
+    case xxlarge    = 48.0
+}
