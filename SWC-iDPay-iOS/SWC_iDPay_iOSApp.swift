7b7bafe5d7d9edd9cd4df7f8d55f9f037eb536a3
stefaniacastiglioni <stefania.castiglioni@mobilesoft.it>
2023-12-06 17:33:44 +0100

    - add AppStateManager in main app - add SplashView at main app opening

--- SWC-iDPay-iOS/SWC_iDPay_iOSApp.swift
+++ SWC-iDPay-iOS/SWC_iDPay_iOSApp.swift
@@ -6,12 +6,27 @@
 //
 
 import SwiftUI
+import PagoPAUIKit
 
 @main
 struct SWC_iDPay_iOSApp: App {
+
+    @StateObject var appManager = AppStateManager()
+
     var body: some Scene {
         WindowGroup {
-            ComponentsDemoListView()
+            ZStack {
+                switch appManager.state {
+                case .splash:
+                    SplashView {
+                        appManager.state = .main
+                    }
+                case .main:
+                    ComponentsDemoListView()
+                }
+            }
+//            .animation(.easeInOut(duration: 2.0), value: appManager.state)
+            .environmentObject(appManager)
         }
     }
 }
