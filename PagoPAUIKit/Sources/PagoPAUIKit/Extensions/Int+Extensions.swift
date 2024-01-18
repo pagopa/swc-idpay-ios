1537b2af837406264574094b5ac35a146d8fdbcd
Stefania Castiglioni <stefania.castiglioni@digitouch.it>
2024-01-18 18:27:42 +0100

    Add NumberPad to UI kit with previews

--- PagoPAUIKit/Sources/PagoPAUIKit/Extensions/Int+Extensions.swift
+++ PagoPAUIKit/Sources/PagoPAUIKit/Extensions/Int+Extensions.swift
@@ -18,4 +18,19 @@ extension Int {
         let number = NSNumber(value: Double(self)/100.0)
         return formatter.string(from: number)!
     }
+    
+    public var formattedAmountNoCurrency: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.decimalSeparator = ","
+        formatter.groupingSeparator = ""
+        formatter.maximumFractionDigits = 2
+        formatter.minimumFractionDigits = 2
+
+        guard self > 0 else {
+            return formatter.string(from: NSNumber(value: 0.0))!
+        }
+        let number = NSNumber(value: Double(self)/100.0)
+        return formatter.string(from: number)!
+    }
 }
