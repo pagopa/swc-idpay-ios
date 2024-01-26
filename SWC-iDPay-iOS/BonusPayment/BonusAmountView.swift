27d99b8002d57b15a5201e0a9e02bc902dde3023
Stefania Castiglioni <stefania.castiglioni@digitouch.it>
2024-01-26 17:06:52 +0100

    - show CIE scan on confirm in BonusAmountView - disable confirm button when amount is 0

--- SWC-iDPay-iOS/BonusPayment/BonusAmountView.swift
+++ SWC-iDPay-iOS/BonusPayment/BonusAmountView.swift
@@ -6,11 +6,14 @@
 //
 import SwiftUI
 import PagoPAUIKit
+import CIEScanner
 
 struct BonusAmountView : View {
     @State var isLoading: Bool = false
     @State var amountText: String = "0,00"
     
+    private var reader = CIEReader()
+
     var body: some View {
         VStack {
             Text("IMPORTO DEL BENE")
@@ -41,10 +44,25 @@ struct BonusAmountView : View {
         CustomLoadingButton(
             buttonType: .primary,
             isLoading: $isLoading) {
-                
+                Task {
+                    do {
+                        let publicKey = try await reader.scan()
+                        print("----- Public key found: ------\n\(String.binToHexRep(publicKey, asArray:true))")
+                    } catch {
+                        guard let cieError = error as? CIEReaderError else { return }
+                        switch cieError {
+                        case .scanNotSupported:
+                            print("NFC not available")
+                        default:
+                            break
+                        }
+                    }
+                }
+
             } label: {
                 Text("Conferma")
             }
+            .disabled(amountText == "0,00")
     }
 }
 
