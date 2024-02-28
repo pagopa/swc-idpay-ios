8d6e00c204dbe7687509b089931488afe183260c
Stefania Castiglioni <stefania.castiglioni@digitouch.it>
2024-02-28 15:11:11 +0100

    Remove login test helper code

--- SWC-iDPay-iOS/Login/LoginViewModel.swift
+++ SWC-iDPay-iOS/Login/LoginViewModel.swift
@@ -10,33 +10,19 @@ final class LoginViewModel: BaseVM {
     
     @Published var username: String = ""
     @Published var password: String = ""
-        
+    
     func login() async throws -> Bool {
-        
-        #if DEBUG
-        if UITestingHelper.isUITesting {
-            print("Wait to login")
-            try? await Task.sleep(nanoseconds: 1 * 4_000_000_000) // 4 second
-            return UITestingHelper.isUserLoginSuccess
-        } else {
-            do {
-                try await networkClient.login(username: username, password: password)
-                return true
-            } catch {
-                return false
-            }
-//            print("Call login in viewmodel with user:\(username), password: \(password)")
-//            try? await Task.sleep(nanoseconds: 1 * 4_000_000_000) // 4 second
-//            return password == "access"
+        do {
+            try await networkClient.login(username: username, password: password)
+            return true
+        } catch {
+            return false
         }
-        #else
-        return false
-        #endif
     }
-
+    
     var isFormValid: Bool {
         !(username.isEmpty || password.isEmpty)
     }
-
+    
 }
 
